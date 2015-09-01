e250d42bb60c42d77eff455da53d373e1cf6d419
charles.labas <cslabas@gmail.com>
2015-09-01 14:44:25 -0400

    Restored files removed in previous commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan
+public class Application {
+
+    @Bean
+    MessageService mockMessageService() {
+        return () -> "Hello, world";
+    }
+
+    @Bean
+    OtherMessageService mockOtherMessageService() {
+        return () -> "Hello, other world";
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+        MessagePrinter printer = context.getBean(MessagePrinter.class);
+        printer.printMessage();
+    }
+}
